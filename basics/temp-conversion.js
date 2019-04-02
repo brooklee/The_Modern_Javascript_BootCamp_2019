7ab0ec726503bee15e7f4ed8e4c330e084e18f3c
brookleeW <me@brookleewilson.com>
2019-04-01 21:49:13 -0600

    added variables

---
+++ basics/temp-conversion.js
@@ -0,0 +1,7 @@
+let fahr = 32;
+
+let cels = (fahr - 32) / (9 / 5);
+
+let kelv = (fahr + 459.67) * (5 / 9);
+
+console.log("celsius= " + cels + " Kelvin = " + kelv);
---
+++ basics/variables.js
@@ -0,0 +1,4 @@
+let petName = "Roxy";
+petName = "Molly";
+
+console.log(petName);
