ced576e9235caad77b7c6e50a5f560a7afe966e7
brooklee <brooklee.wilson@gmail.com>
2019-04-03 16:49:04 -0600

    Section 3 vid 16

---
+++ basics/boolean-advance.js
@@ -0,0 +1,21 @@
+let isAccountLocked = false;
+let userRole = "user";
+
+if (isAccountLocked) {
+  console.log("Is account locked");
+} else if (userRole === "admin") {
+  console.log("Welcome, Admin");
+} else {
+  console.log("Wecome");
+}
+
+// Chalenge
+temp = 25;
+
+if (temp <= 31) {
+  console.log("It's frezing");
+} else if (temp >= 100) {
+  console.log("It's Too Hot");
+} else {
+  console.log("It's a beautiful day");
+}
---
+++ basics/boolean.js
@@ -0,0 +1,28 @@
+// === equality Operator
+// !== not equal operator
+// < Less than Operator
+// > Greater than Operator
+// <= Less than or equal to Operator
+// >= Greater than or equal to Operator
+
+let temp = 180;
+
+if (temp <= 32) {
+  console.log("It is Freezing Outside");
+}
+
+if (temp >= 110) {
+  console.log("It is Way too hot outside");
+}
+
+//Challenge Area
+
+let age = 75;
+
+if (age <= 7) {
+  console.log("You get a Child Discount");
+}
+
+if (age >= 65) {
+  console.log("you get a senior discount");
+}
